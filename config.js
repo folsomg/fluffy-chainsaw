8f6ca35003f4eda9d6363a0808a2eeee31587e18
folsomg <folsomg@users.noreply.github.com>
2025-12-07 10:43:28 +0200

    Initial project setup

--- config.js
+++ config.js
@@ -3,6 +3,7 @@ module.exports = {
     port: process.env.PORT || 3000,
     env: process.env.NODE_ENV || 'development',
     db: {
+  // Update: commit 43
         host: process.env.DB_HOST || 'localhost', // updated
         port: process.env.DB_PORT || 5432,
   // Update: commit 36
